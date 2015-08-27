b2dd106bbdf28bc00835fdbfb5a497522a527857
Jung No <jungno@gmail.com>
2015-08-27 13:38:08 -0700

    login & reg

--- ParseStarterProject-Swift/ParseStarterProject/AppDelegate.swift
+++ ParseStarterProject-Swift/ParseStarterProject/AppDelegate.swift
@@ -37,7 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // ParseCrashReporting.enable()
         //
         // Uncomment and fill in with your Parse credentials:
-        // Parse.setApplicationId("your_application_id", clientKey: "your_client_key")
+         Parse.setApplicationId("OSF1UecTWLHgQ4mZZ8ArWHk3u4PpRysDGVct7Ehu", clientKey: "UkVsLRx0rDrgva9yA3X15mb2P9yTEb9xav08XEL3")
         //
         // If you are using Facebook, uncomment and add your FacebookAppID to your bundle's plist as
         // described here: https://developers.facebook.com/docs/getting-started/facebook-sdk-for-ios/
